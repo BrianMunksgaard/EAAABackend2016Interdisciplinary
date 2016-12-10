216c8b129b9b61cf95b73d6b0b759cd49a7fa6d6
Brian Munksgaard <brian.munksgaard@gmail.com>
2016-12-10 21:50:18 +0100

    Layout and menu.

---
+++ site/bgs/bgs/Areas/Public/Controllers/NavController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace bgs.Areas.Public.Controllers
+{
+    public class NavController : Controller
+    {
+        // GET: Public/Nav
+        public PartialViewResult Menu()
+        {
+            return PartialView();
+        }
+    }
+}
