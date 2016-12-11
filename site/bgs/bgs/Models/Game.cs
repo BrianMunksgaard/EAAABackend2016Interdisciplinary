141480220721db47a71ffc90d6d330ab7dd6a13a
Jens Christian Rasch <jcr@wodecraft.dk>
2016-12-11 13:38:30 +0100

    Added properties to model classes

--- site/bgs/bgs/Models/Game.cs
+++ site/bgs/bgs/Models/Game.cs
@@ -1,11 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace bgs.Models
+﻿namespace bgs.Models
 {
     public class Game
     {
+        #region Private variables
+
+        private int gameId;
+        private string gameName;
+        private string gameCode;
+
+        #endregion
+
+        #region Public properties
+
+        public int GameId
+        {
+            get
+            {
+                return GameId;
+            }
+            set
+            {
+                GameId = value;
+            }
+        }
+
+        public string GameName
+        {
+            get
+            {
+                return GameName;
+            }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    GameName = value;
+                }
+            }
+        }
+
+        public string GameCode
+        {
+            get
+            {
+                return GameCode;
+            }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    GameCode = value;
+                }
+            }
+        }
+
+        #endregion
+
+        public Game()
+        {
+
+        }
     }
 }
