dce41f79bbcb2f2128caa7d6b13a98489454bb80
Jens Christian Rasch <jcr@wodecraft.dk>
2016-12-11 16:36:53 +0100

    Added EntityFramework. Added properties and relationship class.

---
+++ site/bgs/bgs/DAL/BgsContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity;
+
+namespace bgs.DAL
+{
+    public class BgsContext : DbContext
+    {
+
+        public BgsContext() : base("BgsContext")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
---
+++ site/bgs/bgs/Models/ProductGame.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace bgs.Models
+{
+    public class ProductGame
+    {
+        [Key, Column(Order = 0)]
+        public int ProductId { get; set; }
+
+        [Key, Column(Order = 1)]
+        public int GameId { get; set; }
+
+        public virtual Product Product { get; set; }
+
+        public virtual Game Game { get; set; }
+
+        public string Comment { get; set; }
+
+    }
+}
